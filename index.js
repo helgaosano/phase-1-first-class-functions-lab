3bb7aa0170da2745489893a7a522b5d3abc45d53
tania <taniaosano15@gmail.com>
2022-08-21 23:40:48 +0300

    Completed first class functions lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(drivers){
+    return ["Antonia", "Nuru"]
+}
+returnFirstTwoDrivers(drivers)
+
+const returnLastTwoDrivers = function(drivers){
+    return ["Amari", "Mo"]
+}
+returnLastTwoDrivers(drivers)
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(){
+    return function multiplier(x){
+        return 25
+    }
+}
+const fareDoubler = function (createFareMultiplier){
+    return 20;
+}
+
+const fareTripler = function (createFareMultiplier){
+    return 36;
+}
+
+function selectDifferentDrivers([drivers], selectingDrivers){
+    return selectingDrivers(drivers)
+}
